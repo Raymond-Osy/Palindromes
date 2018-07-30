c3299d812522d2674ec7b20911f9449bcf446eb2
Raymond Osy <raymondosy1@gmail.com>
2018-07-30 23:27:52 +0100

    Write JS code for Palindromes

---
+++ app.js
@@ -0,0 +1,12 @@
+function palindromes (str){
+    var removeChar = /[^A-Za-z0-9]/g;
+    var strLC = str.lowerCase();
+    var strTransform = strLC.replace(removeChar, "");
+
+    for (i = 0; i < strTransform.length; i++) {
+        
+    }    
+    
+}
+
+palindromes();
