268ef849f9f96322577446e365e2bd8bdbd2485d
Raymond Osy <raymondosy1@gmail.com>
2018-08-13 20:42:45 +0100

    Add clear button and its functionalities and also commented app.js for understanding

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
+// Get the id's of check and clear button and stored them in a variable
 var checkBtn = document.getElementById('check');
+var clearBtn = document.getElementById('clearBtn');
+//Set the Table to an Empty string
 var currentTable ='';
 
+//Once the check button is clicked, it runs all the code contained within the function
 checkBtn.onclick = function (){
-  var inputs =[];
+    var str = document.getElementById('palindrome').value;  //Get the users input
+    var removeChar = /[^A-Za-z0-9]/g;  //Remove any character not found within the regular expression(regExp)
+    var strTransform = str.toLowerCase().replace(removeChar, "");  //Converts the value to lowercase and replaces the value with RegExp
+    var strRev = strTransform.split('').reverse().join('');  //Splits the value into an array, reverse it and rejoin it
 
-    var str = document.getElementById('palindrome').value;
-    var removeChar = /[^A-Za-z0-9]/g;
-    var strTransform = str.toLowerCase().replace(removeChar, "");
-    var strRev = strTransform.split('').reverse().join('');
+    // Conditions for checking the users input
     if (str === ""){
-      alert("Please input a word or phrase");
-      return false
+      alert("Please input a word or phrase"); //alert for empty input
     }
-    else if (strTransform === strRev){
-      currentTable += `<tr>
-      <td>${strTransform}</td>
-      <td>True</td>
+    else if (strTransform === strRev){ //Check if converted input equals reversed input
+      currentTable +=  //Set input to True and put the input into the empty table
+      `<tr>
+        <td>${str}</td>
+        <td>True</td>
       </tr>`;
-      document.getElementById("tBody").innerHTML = currentTable;
+      document.getElementById("tBody").innerHTML = currentTable;  //Put the table into the tBody element in HTML
     }
-    else {
-      currentTable += `<tr>
-      <td>${strTransform}</td>
-      <td>False</td>
+    else {   //If not...
+      currentTable +=   //Set input to False and put the input into the empty table
+      `<tr>
+        <td>${str}</td>
+        <td>False</td>
       </tr>`;
-      document.getElementById("tBody").innerHTML = currentTable;
+      document.getElementById("tBody").innerHTML = currentTable;  //Put the table into the tBody element in HTML
     }
 
-  //create a formSubmit function below here
-  function addToTable(userInput,status,currentTable){
-
-  }
- 
+    // Clears the table once the clear button is clicked
+    clearBtn.onclick = function (){
+      var x = document.getElementById("tBody");  //Get the tBody HTML element
+      x.parentNode.removeChild(x);  //Removes the table
+      location.reload();  //Reloads the page
+    }
 }
 
