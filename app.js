457851deb91cd85387457ac9845b1efe4f730e22
Raymond Osy <raymondosy1@gmail.com>
2018-08-13 14:41:41 +0100

    Create a table and append values in it with Js and add a little styling for tables visibility

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 var checkBtn = document.getElementById('check');
+var currentTable ='';
 
 checkBtn.onclick = function (){
-  var str = document.getElementById('palindrome').value;
+  var inputs =[];
+
+    var str = document.getElementById('palindrome').value;
     var removeChar = /[^A-Za-z0-9]/g;
     var strTransform = str.toLowerCase().replace(removeChar, "");
     var strRev = strTransform.split('').reverse().join('');
-
-    if (strTransform === strRev){
-      alert("Yea, this is a palindrome");
-      return true
-    }
-    else {
-      alert("Nope, this aint no palindrome");
+    if (str === ""){
+      alert("Please input a word or phrase");
       return false
     }
+    else if (strTransform === strRev){
+      currentTable += `<tr>
+      <td>${strTransform}</td>
+      <td>True</td>
+      </tr>`;
+      document.getElementById("tBody").innerHTML = currentTable;
+    }
+    else {
+      currentTable += `<tr>
+      <td>${strTransform}</td>
+      <td>False</td>
+      </tr>`;
+      document.getElementById("tBody").innerHTML = currentTable;
+    }
+
+  //create a formSubmit function below here
+  function addToTable(userInput,status,currentTable){
+
+  }
+ 
 }
 
